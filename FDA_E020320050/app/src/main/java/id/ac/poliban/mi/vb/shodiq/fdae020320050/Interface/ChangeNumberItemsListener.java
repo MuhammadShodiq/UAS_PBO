71d2317b1c68f86f09f3ae6eb368e2a93a0c019e
MuhammadShodiq <mshodiq21@gmail.com>
2023-01-16 19:10:21 +0800

    first commit

---
+++ FDA_E020320050/app/src/main/java/id/ac/poliban/mi/vb/shodiq/fdae020320050/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vb.shodiq.fdae020320050.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ WisataAppE020320050/app/src/main/java/id/ac/poliban/mi/vb/e020320050/wisataappe020320050/decoration/OnClickLayoutMarginItemListener.java
@@ -0,0 +1,10 @@
+package id.ac.poliban.mi.vb.e020320050.wisataappe020320050.decoration;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnClickLayoutMarginItemListener {
+    void onClick(Context context, View v, int position, int spanIndex, RecyclerView.State state);
+}
